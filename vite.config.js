f1d2624405ecfb516a676259f463ebad0ae7a868
Adam H <Verbatiama@gmail.com>
2026-06-18 22:05:09 +0930

    fix config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "FriendMap",
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
   build: {
     outDir: "assets",
     emptyOutDir: false,
